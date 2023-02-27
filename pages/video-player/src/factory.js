9219b4191c425fbc2f1901ba2fb7d84491004392
barbara carvalho <barbaracarvalho@barbaras-MacBook-Pro.local>
2023-02-27 18:37:07 -0300

    feat: enable button when tf is enabled

--- pages/video-player/src/factory.js
+++ pages/video-player/src/factory.js
@@ -2,6 +2,23 @@ import Controller from "./controller.js"
 import Service from "./service.js"
 import View from "./view.js"
 import Camera from '../../lib/shared/camera.js'
+import { supportsWorkerType } from "../../lib/shared/utils.js"
+
+async function getWorker() {
+  if(supportsWorkerType()) {
+    const worker = new Worker('./src/worker.js', {type: 'module'})
+    return worker
+  }
+
+  const workerMock = {
+    async postMessage() {},
+    onmessage(msg) {}
+  }
+
+  return workerMock
+}
+
+const worker = await getWorker()
 
 const camera = await Camera.init()
 const [rootPath] = window.location.href.split('/pages/')
@@ -9,8 +26,9 @@ const [rootPath] = window.location.href.split('/pages/')
 const factory = {
   async initalize() {
     return Controller.initialize({
-      view: new View({}),
-      service: new Service({})
+      view: new View(),
+      service: new Service({}),
+      worker
     })
   }
 }
